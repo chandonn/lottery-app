c3a69ab4136a420a8ce2d953cf56fcdb99eb6e0c
Alexandre Correa <alexandrecorreadeoliveira@gmail.com>
2022-01-26 18:05:47 -0300

    - Adds example config file

---
+++ blockchain/config example.js
@@ -0,0 +1,26 @@
+// Secret phrase from metamask
+const mnemonicPhrase = ''
+
+// Address to deploy test contracts
+const bscTestnetAddress = 'https://data-seed-prebsc-1-s1.binance.org:8545'
+
+// Address to deploy production contracts
+const bscMainnetAddress = 'https://bsc-dataseed1.binance.org'
+
+// Address to deploy test contracts to Rinkeby testnet
+const rinkebyTestnetAddress = ''
+
+// Address of the deployed test contract
+const bscTestNetContractAddress = ''
+
+// Address of the deployed production contract
+const bscMainnetContractAddress = ''
+
+module.exports = {
+  mnemonicPhrase,
+  bscTestnetAddress,
+  bscMainnetAddress,
+  rinkebyTestnetAddress,
+  bscTestNetContractAddress,
+  bscMainnetContractAddress
+}
